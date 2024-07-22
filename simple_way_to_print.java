163d02b372a865c13b7309704d811a4c8d500306
Rahul5742 <rkgamingchannel8@gmail.com>
2024-07-22 11:03:04 +0545

    Array
    Simple way to print array

---
+++ simple_way_to_print.java
@@ -0,0 +1,22 @@
+public class simple_way_to_print {
+
+        public static void main(String[] args) {
+
+            int [] marks;
+            marks =new int[5];
+            marks[0]=100;
+            marks[1]=70;
+            marks[2]=70;
+            marks[3]=69;
+            marks[4]=98;
+
+            System.out.println(marks [0]);
+            System.out.println(marks [1]);
+            System.out.println(marks [2]);
+            System.out.println(marks [3]);
+            System.out.println(marks [4]);
+
+        }
+    }
+
+
